d1beced6da8da7b4c988b7007eeced233971a157
hohouin000 <66586206+hohouin000@users.noreply.github.com>
2022-12-17 16:26:30 +0800

    updated toast message

--- toast-message.php
+++ toast-message.php
@@ -236,7 +236,7 @@
         <div class="toast align-items-center text-bg-danger border-0" role="alert" aria-live="assertive" aria-atomic="true" id="password-notfound-toast">
             <div class="d-flex">
                 <div class="toast-body">
-                    Password Not Found !
+                    Old Password Not Found !
                 </div>
                 <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
